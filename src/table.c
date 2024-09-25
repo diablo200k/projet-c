1507b85a2b2a0fc065bf6144d6d1816aba8563a4
Mido342 <>
2024-09-25 17:06:27 +0200

    save dans une file
    pas encore tester

--- src/table.c
+++ src/table.c
@@ -33,3 +33,20 @@ void displayusers(User *users, int count){
         printf("ID: %d, Nom: %s, Email: %s, Age: %d\n", users[i].id, users[i].nom, users[i].email, users[i].age);
     }
 }
+
+
+// save user 
+
+
+void saveusertofile(User *users, int count , const char *bdd_users_file){
+    FILE *file = fopen(filename, "w");
+    if (file == NULL) {
+        printf("Error opening file!\n");
+        return;
+    }
+    for (int i=0;i<count;i++){
+        fprintf(file, "%d %s %s %s %d\n", users[i].id, users[i].nom, users[i].email, users[i].mdp, users[i].age);
+    }
+    fclose(file);
+    printf("user saved at %s.\n ",bdd_users_file)
+}
