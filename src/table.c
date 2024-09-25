e1458106c4592c363741248f8017b7d2126e5be2
Mido342 <>
2024-09-25 16:33:09 +0200

    ajouter la structeur de ma table

---
+++ src/table.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+// donner de user 
+
+typedef struct {
+    int id ;
+    char nom [50]; 
+    char email[100];
+    char mdp[50];
+    int age;
+
+}User;
