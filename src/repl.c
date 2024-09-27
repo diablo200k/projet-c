0f0a626f4441618a8c09bb9e7afc11233567ef77
Mido342 <>
2024-09-27 17:06:18 +0200

    help et sup

--- src/repl.c
+++ src/repl.c
@@ -11,6 +11,16 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
         close_input_buffer(input_buffer);
         free_table(table);
         exit(EXIT_SUCCESS);
+    } else if (strcmp(input_buffer->buffer, ".help") == 0) {
+        printf("Instructions d'utilisation :\n");
+        printf("  insert <id> <username> <email>  - Insère une nouvelle ligne\n");
+        printf("  select                          - Affiche toutes les lignes\n");
+        printf("  select where id = <id>          - Affiche une ligne spécifique\n");
+        printf("  update <id> set username=<username> email=<email> - Met à jour une ligne\n");
+        printf("  delete where id = <id>          - Supprime une ligne\n");
+        printf("  .exit                           - Quitte le programme\n");
+        printf("  .help                           - Affiche ces instructions\n");
+        return META_COMMAND_SUCCESS;
     } else {
         return META_COMMAND_UNRECOGNIZED_COMMAND;
     }
@@ -18,6 +28,7 @@ MetaCommandResult do_meta_command(InputBuffer* input_buffer, Table* table) {
 
 void repl(Table* table) {
     InputBuffer* input_buffer = new_input_buffer();
+    printf("Bienvenue dans la base de données simple. Tapez '.help' pour les instructions.\n");
     while (true) {
         print_prompt();
         read_input(input_buffer);
